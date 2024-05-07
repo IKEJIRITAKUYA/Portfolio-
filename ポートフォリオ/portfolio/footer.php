146055401c47755cdb9eeb3d055a3707acd9dc7b
IKEJIRITAKUYA <151913507+IKEJIRITAKUYA@users.noreply.github.com>
2024-05-07 13:16:56 +0900

    Add files via upload

---
+++ ポートフォリオ/portfolio/footer.php
@@ -0,0 +1,6 @@
+<!-- footer -->
+<footer class="footer">
+      <div class="copyright">&copy;IKEJIRI TAKUYA</div>
+      <?php wp_foooter(); ?>
+    </footer>
+    <!-- /footer --> 
---
+++ ポートフォリオ/portfolio/header.php
@@ -0,0 +1,33 @@
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <title>IKEJIRI TAKUYA Portfolio</title>
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="<?php echo get_template_directory_uri(); ?>/https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
+  <link href="<?php echo get_template_directory_uri(); ?>/https://fonts.googleapis.com/css?family=Material+Icons+Outlined" rel="stylesheet">
+  <link href="<?php echo get_template_directory_uri(); ?>/css/ress.css" media="all" rel="stylesheet" type="text/css" />
+  <link href="<?php echo get_template_directory_uri(); ?>/css/my.css" media="all" rel="stylesheet" type="text/css" />
+  <?php wp_head(); ?>
+</head>
+
+<body>
+  <div class="wrapper">
+
+    <!-- header -->
+    <header class="header ">
+      <div class="container">
+        <h1 class="header-logo">
+          <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/img/ロゴのコピー.png" alt=""></a>
+        </h1>
+        <nav class="gnav">
+          <ul class="gnav-list">
+            <li class="gnav-item"><a href="#works">WORKS</a></li>
+            <li class="gnav-item"><a href="#sevice">SERVICE</a></li>
+            <li class="gnav-item"><a href="#skill">SKILL</a></li>
+            <li class="gnav-item"><a href="#about">ABOUT</a></li>
+            <li class="gnav-item"><a href="#contact">CONTACT</a></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+    <!-- /header -->
